c4da6986007f9b4cf1779fbed7c531f18574c067
Kelvin de Assis Ferreira das Neves <kelvin.neves@dataprev.gov.br>
2021-09-24 20:58:03 -0300

    Trigger webhook

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-//app.use('/', routes)
+app.use('/', routes)
 
-routes.get('/', (request, response) => {
-    return response.send('TESTE')
+
+app.listen(PORT, function(){
+    console.log("iniciado...")
 })
-
-app.listen(PORT)
